33e41bb8a08679418a3f321ae17a3f6839aefccf
Tai272620 <tai272620967@gmail.com>
2023-06-30 17:09:01 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import LazyLoad from "./LazyLoad";
+import Navbar from "@components/Navbars/Navbar";
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className="App-container">
+        <Navbar />
+        <Routes>
+          <Route path="" element={LazyLoad(() => import("@pages/Homes/Home"))()}></Route>
+          <Route path="/about" element={LazyLoad(() => import("@pages/Abouts/About"))()}>
+            <Route path="my-infor" element={LazyLoad(() => import("@pages/Abouts/MyInfors/MyInfor"))()} />
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
